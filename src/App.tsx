5b3dbe37182c6870f920a49832c67271e77147ce
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-10-04 14:21:39 +0100

    eslint and prettier are configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import logo from "./assets/crossover.svg";
+import logo from './assets/crossover.svg';
 import { Box, Text } from '@chakra-ui/react';
 
 function App() {
-
   return (
     <Box textAlign={'center'}>
-      <img src={logo} alt="Crossover" height={"200px"} />
+      <img src={logo} alt='Crossover' height={'200px'} />
       <Text fontSize={'2xl'}>
         Crossover Replicate with Reactjs + Storybook + Chakra + TypeScript
       </Text>
     </Box>
-  )
+  );
 }
 
-export default App
+export default App;
